3f9fd9ef1a93c6f6dc011972d4c97a7385434788
Felixnite <141184414+Felixnite@users.noreply.github.com>
2024-01-07 05:03:57 +0000

    cambios en las vistas detalladas

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -11,19 +11,47 @@ export const Single = props => {
 	}, [])
 	return (
 		<div className="container">
-		
-			<div className="card mb-3" style={{maxWidth: "540px"}}>
+
+			<div className="card mb-3" style={{ maxWidth: "1250px" }}>
 				<div className="row g-0">
 					<div className="col-md-4">
-						<img src={"https://starwars-visualguide.com/assets/img/characters/"+params.theid+".jpg"} className="img-fluid rounded-start" alt="..."/>
+						<img src={"https://starwars-visualguide.com/assets/img/characters/" + params.theid + ".jpg"} className="img-fluid rounded-start" alt="..." />
 					</div>
 					<div className="col-md-8">
 						<div className="card-body">
 							<h5 className="card-title">{store.character.name}</h5>
-							<p className="card-text">birth age: {store.character.birth_year}</p>
-							<p className="card-text">gender: {store.character.gender}</p>
-							<p className="card-text">height: {store.character.height}</p>
-							<p className="card-text">mass: {store.character.mass}</p>
+
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi finibus quis tellus facilisis sodales. Aliquam viverra tempor gravida. Phasellus eu mattis ipsum. Integer suscipit consectetur tortor sed placerat. Etiam convallis viverra luctus. Duis mauris nisl, cursus laoreet viverra sed, congue sit amet orci. Nullam diam elit, semper eu posuere in, semper sed ligula. Aliquam nec eros in dui consequat rhoncus ac a massa. Fusce ac dolor sapien. Integer rutrum ut eros sit amet sollicitudin. Fusce dictum risus risus, aliquam fermentum magna ultricies et. Proin lobortis ante nec luctus faucibus. Phasellus eu efficitur augue. Sed volutpat laoreet imperdiet. Cras ultrices sem ut leo efficitur posuere. </p>
+							<hr className="bg-danger" />
+							<div class="container text-center">
+								<div class="row">
+									<div class="col">
+										<p className="card-text text-danger">birth year: {store.character.birth_year}</p>
+									</div>
+									<div class="col">
+										<p className="card-text text-danger">gender: {store.character.gender}</p>
+									</div>
+									<div class="col">
+										<p className="card-text text-danger">height: {store.character.height}</p>
+									</div>
+									<div class="col">
+										<p className="card-text text-danger">mass: {store.character.mass}</p>
+									</div>
+									<div class="col">
+										<p className="card-text text-danger">eye color: {store.character.eye_color}</p>
+									</div>
+									<div class="col">
+										<p className="card-text text-danger">hair color: {store.character.hair_color}</p>
+									</div>
+								</div>
+							</div>
+
+
+
+
+
+
+
 						</div>
 					</div>
 				</div>
--- src/js/views/singlePlanet.js
+++ src/js/views/singlePlanet.js
@@ -12,7 +12,7 @@ export const SinglePlanets = props => {
     return (
         <div className="container">
 
-            <div className="card mb-3" style={{ maxWidth: "540px" }}>
+            <div className="card mb-3" style={{ maxWidth: "1400px" }}>
                 <div className="row g-0">
                     <div className="col-md-4">
                         {params.theid == 1 ?
@@ -22,9 +22,35 @@ export const SinglePlanets = props => {
                     <div className="col-md-8">
                         <div className="card-body">
                             <h5 className="card-title">{store.planet.name}</h5>
-                            <p className="card-text">climate: {store.planet.climate}</p>
-                            <p className="card-text">diameter: {store.planet.diameter}</p>
-                            <p className="card-text">gravity: {store.planet.gravity}</p>
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi finibus quis tellus facilisis sodales. Aliquam viverra tempor gravida. Phasellus eu mattis ipsum. Integer suscipit consectetur tortor sed placerat. Etiam convallis viverra luctus. Duis mauris nisl, cursus laoreet viverra sed, congue sit amet orci. Nullam diam elit, semper eu posuere in, semper sed ligula. Aliquam nec eros in dui consequat rhoncus ac a massa. Fusce ac dolor sapien. Integer rutrum ut eros sit amet sollicitudin. Fusce dictum risus risus, aliquam fermentum magna ultricies et. Proin lobortis ante nec luctus faucibus. Phasellus eu efficitur augue. Sed volutpat laoreet imperdiet. Cras ultrices sem ut leo efficitur posuere. </p>
+
+                            <div className="container text-center">
+                                <hr className="bg-danger" />
+                                <div className="row">
+                                    <div class="col">
+                                        <p className="card-text text-danger">climate: {store.planet.climate}</p>
+                                    </div>
+                                    <div class="col">
+                                        <p className="card-text text-danger">diameter: {store.planet.diameter}</p>
+                                    </div>
+                                    <div class="col">
+                                        <p className="card-text text-danger">gravity: {store.planet.gravity}</p>
+                                    </div>
+                                    <div class="col">
+                                        <p className="card-text text-danger">population: {store.planet.population}</p>
+                                    </div>
+                                    <div>
+                                        <p className="card-text text-danger">orbital period: {store.planet.orbital_period}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+
+
+
 
                         </div>
                     </div>
